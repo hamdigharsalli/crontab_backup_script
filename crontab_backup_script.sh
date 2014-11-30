14508908f6f625d5a9a7ec30677946cffefc2c0d
jloughry <joe.loughry@gmail.com>
2014-11-30 14:05:55 -0700

    wait longer for the remote machine to go to sleep; make pings visible in the report

--- crontab_backup_script.sh
+++ crontab_backup_script.sh
@@ -24,7 +24,7 @@ initialise_variables()
 	report_to_email_address=$private_email_address_to_send_report_to
 	from_email_address=cron
 
-	script_version=86
+	script_version=87
 
 	#
 	# Note that only alphanumeric characters and underscores are allowed
@@ -322,6 +322,7 @@ determine_state_of_remote_machine()
 		sleep 30
 	fi
 
+    report "(ping)"
 	$ping_command $m
 	if [ $? -eq 0 ]; then
 		report "The remote machine $m is up."
@@ -575,6 +576,7 @@ check_free_space_on_remote_machine()
 	#
 	# Only try SSH if ping works first.
 	#
+    report "(ping)"
 	$ping_command $machine
 	
 	if [ $? -eq 0 ]; then
@@ -592,6 +594,7 @@ put_remote_machine_back_to_sleep()
 {
     user_at_machine=$1
 	machine=`echo $user_at_machine | cut -d @ -f 2`
+    report "(ping)"
     $ping_command $machine
     if [ $? -eq 0 ]; then
         $ssh_command -i /Users/$backup_username/.ssh/id_rsa \
@@ -948,7 +951,11 @@ blank_line
 check_free_space_on_remote_machine $private_M_machine
 
 put_remote_machine_back_to_sleep $private_M_machine
-sleep 40
+sleep 60
+determine_state_of_remote_machine MKL.local
+sleep 60
+determine_state_of_remote_machine MKL.local
+sleep 60
 determine_state_of_remote_machine MKL.local
 
 #
