8510244abd3173320f1248a841765cd110498c8e
jloughry <joe.loughry@gmail.com>
2015-03-16 13:57:13 -0600

    remove unnecessary blank line

--- crontab_backup_script.sh
+++ crontab_backup_script.sh
@@ -24,7 +24,7 @@ initialise_variables()
 	report_to_email_address=$private_email_address_to_send_report_to
 	from_email_address=cron
 
-	script_version=147
+	script_version=148
 
 	#
 	# Note that only alphanumeric characters and underscores are allowed
@@ -1068,8 +1068,6 @@ graceful_exit()
 {
 	figure_overall_success_code
 	compute_statistics
-
-	blank_line
 
 	format_report
 
