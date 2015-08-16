e8b21a0d0e901aa4c27c0dd6196eea8323484553
jloughry <joe.loughry@gmail.com>
2015-08-15 21:38:03 -0600

    syntax error in cut -c range value; also, report() doesn't work for backtick expressions

--- crontab_backup_script.sh
+++ crontab_backup_script.sh
@@ -24,7 +24,7 @@ initialise_variables()
 	report_to_email_address=$private_email_address_to_send_report_to
 	from_email_address=cron
 
-	script_version=168
+	script_version=169
 
 	#
 	# Note that only alphanumeric characters and underscores are allowed
@@ -780,8 +780,8 @@ function dim_display_on_remote_machine
 
         $ssh_command -i /Users/$backup_username/.ssh/id_rsa \
             $user_at_machine \
-            "pmset -g log | grep -i \"display is\" | tail -1 | cut -c 48--99" \
-            >> $tempfile 2>&1
+            "pmset -g log | grep -i \"display is\" | tail -1 | cut -c 48-99" \
+                >> $tempfile 2>&1
     fi
 }
 
@@ -793,7 +793,8 @@ function dim_display_on_local_machine
 {
     report "Dimming the display on this machine."
     pmset displaysleepnow
-    report `pmset -g log | grep -i \"display is\" | tail -1 | cut -c 48--99`
+    pmset -g log | grep -i \"display is\" | tail -1 | cut -c 48-99 \
+        >> $tempfile 2>&1
 }
 
 check_for_existence_of_all_backup_volumes()
