3cc41f1d3452d7a85bc32ba6a0c8d3125e899ef1
jloughry <joe.loughry@gmail.com>
2015-09-03 06:53:06 -0600

    check state of display before and after dimming

--- crontab_backup_script.sh
+++ crontab_backup_script.sh
@@ -24,7 +24,7 @@ initialise_variables()
 	report_to_email_address=$private_email_address_to_send_report_to
 	from_email_address=cron
 
-	script_version=170
+	script_version=171
 
 	#
 	# Note that only alphanumeric characters and underscores are allowed
@@ -772,11 +772,19 @@ function dim_display_on_remote_machine
 
     $ping_command $machine
     if [ $? -eq 0 ]; then
+        # First query if the display is on or off currently.
+
+        $ssh_command -i /Users/$backup_username/.ssh/id_rsa \
+            $user_at_machine \
+            "pmset -g log | grep -i \"display is\" | tail -1 | cut -c 48-99" \
+                >> $tempfile 2>&1
+
         report "Dimming the display on $machine"
+
         $ssh_command -i /Users/$backup_username/.ssh/id_rsa \
             $user_at_machine "pmset displaysleepnow" >> $tempfile 2>&1
 
-        # Determine if the screen really is off now.
+        # Now see if the screen really is off.
 
         $ssh_command -i /Users/$backup_username/.ssh/id_rsa \
             $user_at_machine \
